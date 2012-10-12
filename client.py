eb91c2ed336bcfa587e714f7f62108f32c1ce24b
Sam Rogerson <samr@cern.ch>
2012-10-12 05:00:03 +0100

    add function to print to room (only on stdin atm) and print all commands it doesn't understand

--- client.py
+++ client.py
@@ -5,27 +5,26 @@ import socket
 import select
 import sys
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect(('127.0.0.1',8000))
-
-# Add stdin to the rset so that we know when to read from the user
-rset = [ s, sys.stdin ]
-
-# Null wset
-wset = []
-
 available_rooms = set()
 current_room = ""
 
+def show_message(room, sender, message):
+    if room == current_room:
+        print "{u}: {m}".format(u=sender, m=message)
+    # TODO: else append to some file: needs to be efficient though
+
 def is_server_message(message):
     return message[0] == "%"
+
+def parse_server_message(message):
+    return message[0], message[1], message[2:]
 
 def is_server_command(message):
     return message[0] == "!"
 
 def parse_server_command(message):
     return message[1:].split()
+
 
 def notify_client(f):
     def notify():
@@ -50,12 +49,27 @@ def join_room(room):
     add_room(room)
     current_room = room
 
+def print_command(*args):
+    show_message(current_room, "SERVER", " ".join(args))
 
-commands = {
+
+commands=defaultdict(print_command)
+commands.update({
         'join': join_room,
         'avail+': add_room,
         'avail-': remove_room,
-        }
+        }}
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('127.0.0.1',8000))
+
+# Add stdin to the rset so that we know when to read from the user
+rset = [ s, sys.stdin ]
+
+# Null wset
+wset = []
 
 try:
     data = s.recv(4096)
@@ -88,8 +102,8 @@ try:
                     command_args = parsed_command[1:]
                     commands[command](*command_args)
                 else:
-                    # TODO: need to receive room from server and print or log
-                    print data
+                    target_room, sender, message = parse_server_message(data)
+                    show_message(target_room, sender, message)
 
 except KeyboardInterrupt:
     pass
